1bd6eed530d15552e837713db6d73dcfd35c26b4
Steven Martinez Pelaez <smartinezpelaez@gmail.com>
2020-05-03 20:05:44 -0500

    Agregar archivos de proyecto.

---
+++ Avianca/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Avianca.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
